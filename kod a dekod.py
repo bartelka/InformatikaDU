001657360ad3d4622ad155adb6cce85feee7414d
bartelka <tanicka879gmail.com>
2022-11-08 23:04:03 +0100

    hotovo

--- kod a dekod.py
+++ kod a dekod.py
@@ -1,17 +1,17 @@
 def kodovanie(retazec:str, kod:str)->str:
     novy_retazec = ""
     x = 0
-    kodicek = kod
+    kd = kod
     for i in range(len(retazec)):
-        while len(retazec) != len(kodicek):
-            kodicek += kod[x]
+        while len(kd) != len(retazec):
+            kd += kod[x]
             x += 1
             if x == (len(kod)-1):
                 x = 0
         a = ord(retazec[i])
-        b = ord(kodicek[i])
-        p = a + b
-        p = chr(p)
-        novy_retazec += p
+        b = ord(kd[i])
+        vys = (((a-97)+b)%26)+97
+        vys = chr(vys)
+        novy_retazec += vys
     return novy_retazec
 print(kodovanie("ahoj svet", "kvet"))
